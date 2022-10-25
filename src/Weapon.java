32323f0163a44cfbac6127668eac658d510dc4f2
Balasagyn <btalantov16@gmail.com>
2022-10-25 17:30:21 +0600

    1

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Weapon {
+public class Weapon {
+    private WeaponType weaponType;
+    private String name;
+
+    public WeaponType getWeaponType() {
+        return weaponType;
+    }
+
+    public void setWeaponType(WeaponType weaponType) {
+        this.weaponType = weaponType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
